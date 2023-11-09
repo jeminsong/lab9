7ab098f168d09a5a981f01dac038a2a3ef2cf9dd
jeminsong <143300705+jeminsong@users.noreply.github.com>
2023-11-08 16:13:45 -0800

    finished part-2

--- part-2/schedule.cc
+++ part-2/schedule.cc
@@ -1,45 +1,38 @@
-// TODO: Add the required header
+// Jemin Song
+// jeminsong@csu.fullerton.edu
+// @jeminsong
+// Partners: @lybrenda
 
 #include <iostream>
 #include <string>
 
-// Boolean function with two parameter strings that returns true
-// if the strings do not contain an 'e' at the same index
-// and false if the strings contain an 'e' at the same index.
 bool IsValid(const std::string& schedule1, const std::string& schedule2) {
-  // TODO: Declare a local variable of type Bool and initialize it with true
-
-  // TODO: Consider a loop to examine each character at index i in
-  // both strings; if both strings have 'e' at the same index,
-  // then change the Bool variable to false
-  // Since the two strings may have different length, compare them
-  // index by index until one of them is finished
-
-  // TODO: Return the local variable
-
-  // TODO: Remove the return statement below
-
-  return false;
+  bool variable = true;
+  if (schedule1.size() != schedule2.size()) {
+    return false;
+  }
+  for (int i = 0; i < schedule1.size(); ++i) {
+    if (schedule1[i] == 'e' && schedule2[i] == 'e') {
+      variable = false;
+      break;
+    }
+  }
+  return variable;
 }
-
 int main(int argc, char const* argv[]) {
-  // TODO: Declare a variable to hold the first schedule
+  std::string first_schedule;
+  std::string second_schedule;
+  std::cout << "Please enter the schedule of Philosopher 1: ";
+  std::cin >> first_schedule;
+  std::cout << "Please enter the Schedule of Philosopher 2: ";
+  std::cin >> second_schedule;
 
-  // TODO: print the prompt "Please enter the schedule of the Philosopher 1: "
-  // and request for input a number from standard input into the variable.
+  bool is_valid = IsValid(first_schedule, second_schedule);
 
-  // TODO: Declare a variable to hold the first schedule
-
-  // TODO: Print the prompt "Schedule of Philosopher 2: "
-  // and request for input a number from standard input into the variable.
-
-  // TODO: Declare a Boolean variable
-
-  // TODO: Assign to the Boolean variable the value returned from
-  // a call to the IsValid function above.
-
-  // TODO If the variable above is true, print out "Valid schedule",
-  // else print out "Invalid schedule"
-  // then a newline character.
+  if (is_valid) {
+    std::cout << "Valid schedule\n";
+  } else {
+    std::cout << "Invalid schedule\n";
+  }
   return 0;
 }
